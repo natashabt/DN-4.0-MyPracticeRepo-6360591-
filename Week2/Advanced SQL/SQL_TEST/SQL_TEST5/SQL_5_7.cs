5439e59ac1c6f43665adb138c3a6e5f6a16f8235
Shrinkhalakumari <149521671+natashabt@users.noreply.github.com>
2025-06-30 21:41:25 +0530

    Add files via upload

---
+++ Week2/Advanced SQL/SQL_TEST/SQL_TEST5/SQL_5_7.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.SqlClient;
+
+
+
+namespace SQL_TEST5
+{
+    internal class SQL_5_7
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=localhost\\SQLEXPRESS;Database=TEST01;Trusted_Connection=True;Encrypt=False;";
+
+            Console.WriteLine("Enter EmployeeID to get Annual Salary:");
+            int employeeId = int.Parse(Console.ReadLine());
+
+            string query = "SELECT dbo.fn_CalculateAnnualSalary(@EmployeeID) AS AnnualSalary;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@EmployeeID", employeeId);
+
+                object result = command.ExecuteScalar();
+
+                if (result != DBNull.Value)
+                {
+                    Console.WriteLine($"Annual Salary for EmployeeID {employeeId}: {result}");
+                }
+                else
+                {
+                    Console.WriteLine("Employee not found.");
+                }
+            }
+        }
+    }
+}
