2452fd2ac8d0a01bda45a96a791bdabc4bede615
Shrinkhalakumari <149521671+natashabt@users.noreply.github.com>
2025-07-06 15:04:00 +0530

    Add files via upload

---
+++ Week3/Entity_Framework_Core/EF_Core_8.0_HQL/Lab3_EFCoreCLI_Migration/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab3_EFCoreCLI_Migration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
