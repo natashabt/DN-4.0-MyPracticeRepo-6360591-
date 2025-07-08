736abbe09337238debceba95871e8d81c9e1059e
Shrinkhalakumari <149521671+natashabt@users.noreply.github.com>
2025-07-09 00:16:06 +0530

    Add files via upload

---
+++ Week2/Moq/UserNotification.Tests/UserNotifierTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+using Moq;
+using UserNotificationLib;
+
+namespace UserNotification.Tests
+{
+    [TestFixture]
+    public class UserNotifierTests
+    {
+        private UserNotifier _userNotifier;
+        private Mock<IMessageSender> _mockMessageSender;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _mockMessageSender = new Mock<IMessageSender>();
+
+            _mockMessageSender
+                .Setup(x => x.SendMessage(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(true);
+
+            _userNotifier = new UserNotifier(_mockMessageSender.Object);
+        }
+
+        [Test]
+        public void NotifyUser_WhenCalled_ReturnsTrue()
+        {
+            var result = _userNotifier.NotifyUser();
+
+            Assert.That(result, Is.True);
+        }
+    }
+}
+
