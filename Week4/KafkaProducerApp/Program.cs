9f6c432bb28569a562aeaf71a91a28fc74f3e365
Shrinkhalakumari <149521671+natashabt@users.noreply.github.com>
2025-07-13 21:24:03 +0530

    Add files via upload

---
+++ Week4/KafkaProducerApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using Confluent.Kafka;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+        using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+        Console.WriteLine("Enter messages to send to Kafka (type 'exit' to stop):");
+
+        while (true)
+        {
+            var message = Console.ReadLine();
+            if (message == "exit") break;
+
+            await producer.ProduceAsync("chat-app", new Message<Null, string> { Value = message });
+            Console.WriteLine("Message sent: " + message);
+        }
+    }
+}
+
