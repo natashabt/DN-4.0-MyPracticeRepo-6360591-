97afe15ce2b138fe410c53a56fb290b2d8d3eece
Shrinkhalakumari <149521671+natashabt@users.noreply.github.com>
2025-07-18 20:31:02 +0530

    Add files via upload

---
+++ Week5/Microservices-JWT/SecureUserAPI_JWT/Controllers/AuthController.cs
@@ -0,0 +1,60 @@
+﻿using JWTAuthDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JWTAuthDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginModel model)
+        {
+            if (IsValidUser(model))
+            {
+                var token = GenerateJwtToken(model.Username);
+                return Ok(new { Token = token });
+            }
+            return Unauthorized();
+        }
+
+        private bool IsValidUser(LoginModel model)
+        {
+            // Dummy user check. Replace with real user validation
+            return model.Username == "admin" && model.Password == "1234";
+        }
+
+        private string GenerateJwtToken(string username)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(60),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
+
---
+++ Week5/Microservices-JWT/SecureUserAPI_JWT/Controllers/SecureController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTAuthDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SecureController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet("secret")]
+        public IActionResult GetSecret()
+        {
+            var username = User.Identity?.Name;
+            return Ok($"Welcome, {username}! This is a protected message.");
+        }
+    }
+}
+
