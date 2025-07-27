5f7bc56e39e2ea5ced96199cc44cdf7d6671bb35
Shrinkhalakumari <149521671+natashabt@users.noreply.github.com>
2025-07-27 20:04:17 +0530

    Add files via upload

---
+++ Week6/BloggerApp/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import BookDetails from './components/BookDetails';
+import BlogDetails from './components/BlogDetails';
+import CourseDetails from './components/CourseDetails';
+
+function App() {
+  return (
+    <div className="container">
+      <CourseDetails />
+      <BookDetails />
+      <BlogDetails />
+    </div>
+  );
+}
+
+export default App;
+
+
